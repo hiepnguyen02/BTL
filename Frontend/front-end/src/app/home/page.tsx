2445f2abc3018d2b9ac117ee318eb267fdfdd3e1
hiepnguyen02 <hiepnguyen.uet.vnu@gmail.com>
2023-10-19 15:11:23 +0700

    -frontend-hometab

--- Frontend/front-end/src/app/home/page.tsx
+++ Frontend/front-end/src/app/home/page.tsx
@@ -2,7 +2,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-import {Button, Col, Form, Image, Alert} from "react-bootstrap";
+import {Button, Col, Form, Image, Alert, Navbar, NavbarToggle, NavItem, Nav, Tabs, Tab} from "react-bootstrap";
 import loginImage from '../../img/login/login.png'
 import dictionary from '../../img/login/dictionary.png'
 import {auto} from "@popperjs/core";
@@ -14,6 +14,13 @@ import {registerService} from "@/service/AuthenticationService/registerService";
 import {set} from "zod";
 import {router} from "next/client";
 import {useRouter} from "next/navigation";
+import homeIcon from '../../img/home/homeIcon.png'
+import dashBoardIcon from '../../img/home/dashboard.png'
+import translationIcon from '../../img/home/translation.png'
+import settingsIcon from '../../img/home/settings.png'
+import bookIcon from '../../img/home/book.png'
+import editIcon from '../../img/home/edit.png'
+import userIcon from '../../img/home/user.png'
 
 export default function Page() {
     const [userRegister, setUserRegister] = useState<UserRegister | null>({
@@ -37,16 +44,92 @@ export default function Page() {
         console.log("djhdjdhj");
     }
     return (
+        <Container
+            fluid={true}
+            style={{
+                background: "pink",
+                overflow: "hidden",
+                height: "100vh",
+                padding: 20,
+            }}>
 
-        <Container fluid={"lg"}>
-            <Row>
-                <Col>
-                    <img
+            <Container className={"bg-white"}
+                       fluid={true}
+                       style={{
+                           padding: 20,
+                           borderRadius: 30,
+                           height: "100vh",
 
-                </Col>
-            </Row>
+                       }}>
+                <Row>
+                    <Col md={9}>
+                        <Tab.Container id="left-tabs-example" defaultActiveKey="dashBoard">
+                            <Row>
+                                <Col md={3}>
+                                    <img src={homeIcon.src} style={{marginTop: 10, width: 80}}/>
+                                    <text style={{marginLeft: 10}} className={"fw-bolder h6 font-monospace"}>Study with
+                                        me!
+                                    </text>
+
+                                    <Nav variant="pills" className="flex-column mt-4">
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="dashBoard"
+                                                      style={{fontSize: 18, fontFamily: "monospace"}}>
+                                                <img src={dashBoardIcon.src} style={{width: 30, marginRight: 10}}/>
+                                                Dashboard</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="translate"
+                                                      style={{fontSize: 18, fontFamily: "monospace"}}>
+                                                <img src={translationIcon.src} style={{width: 30, marginRight: 10}}/>
+                                                Translate</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="dictionary"
+                                                      style={{fontSize: 18, fontFamily: "monospace"}}>
+                                                <img src={bookIcon.src} style={{width: 30, marginRight: 10}}/>
+                                                Dictionary</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="setting"
+                                                      style={{fontSize: 18, fontFamily: "monospace"}}>
+                                                <img src={settingsIcon.src} style={{width: 30, marginRight: 10}}/>
+                                                Settings</Nav.Link>
+                                        </Nav.Item>
+
+                                    </Nav>
+                                </Col>
+                                <Col md={6}>
+                                    <Tab.Content>
+                                        <Tab.Pane eventKey="dashBoard">First tab content</Tab.Pane>
+                                        <Tab.Pane eventKey="translate">Second tab content</Tab.Pane>
+                                        <Tab.Pane eventKey="dictionary">Second tab content</Tab.Pane>
+                                        <Tab.Pane eventKey="setting">Second tab content</Tab.Pane>
+                                    </Tab.Content>
+                                </Col>
+                            </Row>
+                        </Tab.Container>
+
+                    </Col>
+
+                    <Col md={3}>
+                        <Row className={"mt-4 justify-content-between"}>
+                            <Col xs={4}>
+                                <text style={{marginLeft: 10}} className={"fw-bolder h6 font-monospace"}>Profile
+                                </text>
+                            </Col>
+                            <Col xs={2}>
+                                <img src={editIcon.src} style={{width: 30}}/>
+                            </Col>
+                        </Row>
+                        <Row xs={1} className={"justify-content-center mt-5"}>
+                            <img src={userIcon.src} style={{width: 160}}/>
+                        </Row>
+                    </Col>
+                </Row>
 
 
+            </Container>
         </Container>
     )
 }
